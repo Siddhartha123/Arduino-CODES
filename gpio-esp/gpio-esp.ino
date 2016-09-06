3f817c4990d0bb81fe916fbaca45a82a4b827c24
I Siddhartha Kumar <iskumar789@gmail.com>
2016-09-06 17:56:07 +0530

    gpio
    tried in mega with Serial2

--- gpio-esp/gpio-esp.ino
+++ gpio-esp/gpio-esp.ino
@@ -1,19 +1,22 @@
-
-#include <SoftwareSerial.h>
+#define SERIAL_BUFFER_SIZE 512
 #include <ESP8266.h>
 
-SoftwareSerial mySerial(2,3); /* RX:D2, TX:D3 */
-ESP8266 wifi(mySerial);
+
+ESP8266 wifi(Serial2);
 
 void setup(void)
 {
- wifi.GPIO(1,HIGH);
- delay(1000);
- wifi.GPIO(1,LOW);
- delay(1000);
+  
 }
 
 void loop(void)
 {
+while(1)
+{
+ wifi.GPIO(1,LOW);
+ delay(1000);
+  wifi.GPIO(1,HIGH);
+ delay(1000);
+}
 }
 
