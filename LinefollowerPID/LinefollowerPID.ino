46b081ae7c37c48d55dfb393ffaef99894164371
I Siddhartha Kumar <iskumar789@gmail.com>
2016-10-21 19:23:27 +0530

    new code with PID

--- LinefollowerPID/LinefollowerPID.ino
+++ LinefollowerPID/LinefollowerPID.ino
@@ -1,95 +1,101 @@
-#define max_speed 255
-#define motor_left 3
-#define motor_right 5
+#include <QTRSensors.h>
 
-bool l1,l2,r1,r2,temp;
-int sensors[5];
-int right_speed,left_speed;
-float set_point;
-float Kp,Ki,Kd;
-int sensors_average,sensors_sum;
-float proportional,derivative,integral,last_proportional;
-int error_value,pos;
+#define Kp 0 // experiment to determine this, start by something small that just makes your bot follow the line at a slow speed
+#define Kd 0 // experiment to determine this, slowly increase the speeds and adjust this value. ( Note: Kp < Kd) 
+#define rightMaxSpeed 200 // max speed of the robot
+#define leftMaxSpeed 200 // max speed of the robot
+#define rightBaseSpeed 150 // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define leftBaseSpeed 150  // this is the speed at which the motors should spin when the robot is perfectly on the line
+#define NUM_SENSORS  6     // number of sensors used
+#define TIMEOUT       2500  // waits for 2500 us for sensor outputs to go low
+#define EMITTER_PIN   2     // emitter is controlled by digital pin 2
+
+#define rightMotor1 3
+#define rightMotor2 4
+#define rightMotorPWM 5
+#define leftMotor1 12
+#define leftMotor2 13
+#define leftMotorPWM 11
+#define motorPower 8
+
+QTRSensorsRC qtrrc((unsigned char[]) {  14, 15, 16, 17, 18, 19} ,NUM_SENSORS, TIMEOUT, EMITTER_PIN); // sensor connected through analog pins A0 - A5 i.e. digital pins 14-19
+
+unsigned int sensorValues[NUM_SENSORS];
 
 void setup()
 {
+  pinMode(rightMotor1, OUTPUT);
+  pinMode(rightMotor2, OUTPUT);
+  pinMode(rightMotorPWM, OUTPUT);
+  pinMode(leftMotor1, OUTPUT);
+  pinMode(leftMotor2, OUTPUT);
+  pinMode(leftMotorPWM, OUTPUT);
+  pinMode(motorPower, OUTPUT);
   
-pinMode(3,OUTPUT);
-pinMode(5,OUTPUT);
-}
+  int i;
+for (int i = 0; i < 100; i++) // calibrate for sometime by sliding the sensors across the line, or you may use auto-calibration instead
+
+  /* comment this part out for automatic calibration 
+  if ( i  < 25 || i >= 75 ) // turn to the left and right to expose the sensors to the brightest and darkest readings that may be encountered
+     turn_right();  
+   else
+     turn_left(); */ 
+   qtrrc.calibrate();   
+   delay(20);
+wait();  
+delay(2000); // wait for 2s to position the bot before entering the main loop 
+    
+    /* comment out for serial printing
+    
+    Serial.begin(9600);
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+      Serial.print(qtrrc.calibratedMinimumOn[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+
+    for (int i = 0; i < NUM_SENSORS; i++)
+    {
+      Serial.print(qtrrc.calibratedMaximumOn[i]);
+      Serial.print(' ');
+    }
+    Serial.println();
+    Serial.println();
+    */
+  } 
+
+int lastError = 0;
 
 void loop()
 {
-    sensors_read(); //Reads sensor values and computes sensor sum and weighted average
-    pid_calc(); //Calculates position[set point] and computes Kp,Ki and Kd
-    calc_turn(); //Computes the error to be corrected
-    motor_drive(right_speed,left_speed); //Sends PWM signals to the motors
-}
-void motor_drive(int right_speed, int left_speed)
-{ // Drive motors according to the calculated values for a turn
-analogWrite(motor_right, right_speed);
-analogWrite(motor_left, left_speed);
-delay(50); // Optional
-}
+  unsigned int sensors[6];
+  int position = qtrrc.readLine(sensors); // get calibrated readings along with the line position, refer to the QTR Sensors Arduino Library for more details on line position.
+  int error = position - 2500;
 
+  int motorSpeed = Kp * error + Kd * (error - lastError);
+  lastError = error;
 
-void calc_turn()
-{ //Restricting the error value between -256 and +256.
-    if (error_value< -256)
-        error_value = -256;
-
-    if (error_value> 256)
-        error_value = 256;
-
-// If error_value is less than zero calculate right turn speed values
-    if (error_value< 0)
-    {
-        right_speed = max_speed + error_value;
-        left_speed = max_speed;
-    }
-// If error_value is greater than zero calculate left turn values
-    else
-    {
-        right_speed = max_speed;
-        left_speed = max_speed - error_value;
-    }
-}
-
-void sensors_read()
-{
-    sensors_sum = 0;
-    for (int i = 0; i < 5; i++)
-    {
-      sensors[i] = analogRead(i);
-      sensors_average += sensors[i] * i * 1000; //Calculating the weighted mean of the sensor readings
-      sensors_sum += int(sensors[i]); //Calculating sum of sensor readings
-    }
-}
-
-void pid_calc()
-{ 
-    pos = sensors_average / sensors_sum;
-    //proportional = pos – set_point;
-    integral = integral + proportional;
-    derivative = proportional - last_proportional;
-    last_proportional = proportional;
-    error_value =proportional * Kp + integral * Ki + derivative * Kd;
-}
-void reverse_sense(char a[])
-{
-   if(a=="left")
+  int rightMotorSpeed = rightBaseSpeed + motorSpeed;
+  int leftMotorSpeed = leftBaseSpeed - motorSpeed;
+  
+    if (rightMotorSpeed > rightMaxSpeed ) rightMotorSpeed = rightMaxSpeed; // prevent the motor from going beyond max speed
+  if (leftMotorSpeed > leftMaxSpeed ) leftMotorSpeed = leftMaxSpeed; // prevent the motor from going beyond max speed
+  if (rightMotorSpeed < 0) rightMotorSpeed = 0; // keep the motor speed positive
+  if (leftMotorSpeed < 0) leftMotorSpeed = 0; // keep the motor speed positive
+  
    {
-      temp=l1;
-      l1=l2;
-      l2=temp;
-   }
-   
-   if(a=="right")
-   {
-      temp=r1;
-      r1=r2;
-      r2=temp;
-   }
-
+  digitalWrite(motorPower, HIGH); // move forward with appropriate speeds
+  digitalWrite(rightMotor1, HIGH);
+  digitalWrite(rightMotor2, LOW);
+  analogWrite(rightMotorPWM, rightMotorSpeed);
+  digitalWrite(motorPower, HIGH);
+  digitalWrite(leftMotor1, HIGH);
+  digitalWrite(leftMotor2, LOW);
+  analogWrite(leftMotorPWM, leftMotorSpeed);
+}
 }
   
+void wait(){
+    digitalWrite(motorPower, LOW);
+}
