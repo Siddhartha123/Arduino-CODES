bfd4f07694e1d4046821f7cf82dacf37f83e4212
I Siddhartha Kumar <iskumar789@gmail.com>
2016-12-03 15:17:53 +0530

    ...

---
+++ Mesh_Flare/sensor.ino
@@ -0,0 +1,44 @@
+int read_sensor()
+{
+  for(i=0;i<7;i++)
+    sensor_read[i]=analogRead(i+14);
+}
+void process_sensor()
+{
+  for(i=0;i<7;i++)
+      sensor[i]=sensor_read[i]>512?1:0;
+      
+  for(int i=0; i<7; i++) 
+    {
+     if(sensor[i])
+        activeSensor++;
+     totalSensor += sensor[i] * (i+1);
+    }
+    avgSensor = totalSensor/activeSensor;
+    activeSensor = 0; totalSensor = 0;
+}
+void PID_program()
+{ 
+    read_sensor();
+    
+    previousError = error;                                  // save previous error for differential 
+    error = avgSensor - 4.5;                               // Count how much robot deviate from center
+    totalError += error;                                      // Accumulate error for integral
+    power = kp*error + kd*(error-previousError) + ki*totalError;
+    
+    if( power>255 ) { power = 255.0; }
+    if( power<-255.0 ) { power = -255.0; }
+    
+    if(power<0) // Turn left
+    {
+      PWM_Right = 255;
+      PWM_Left = 255 - abs(int(power));
+    }
+    
+    else // Turn right
+    {
+      PWM_Right = 255 - int(power);
+      PWM_Left = 255;
+    }
+   
+}
